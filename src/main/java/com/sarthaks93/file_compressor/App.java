4e7485864ae7825948c6f7deda225b60bb833792
sarthak <sarthaks93@gmail.com>
2019-02-19 23:29:51 +0530

    maven project initiated

---
+++ src/main/java/com/sarthaks93/file_compressor/App.java
@@ -0,0 +1,13 @@
+package com.sarthaks93.file_compressor;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
