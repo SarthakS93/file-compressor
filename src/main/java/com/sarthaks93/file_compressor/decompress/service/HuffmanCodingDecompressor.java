a37ddacd5add05e827eac6d9497b78386e9ef0b7
sarthak <sarthaks93@gmail.com>
2019-02-26 23:56:05 +0530

    started decompress method

--- src/main/java/com/sarthaks93/file_compressor/decompress/service/HuffmanCodingDecompressor.java
+++ src/main/java/com/sarthaks93/file_compressor/decompress/service/HuffmanCodingDecompressor.java
@@ -1,17 +1,70 @@
 package com.sarthaks93.file_compressor.decompress.service;
 
+import java.util.BitSet;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.sarthaks93.file_compressor.compressor.service.HuffmanCodingCompressor;
 import com.sarthaks93.file_compressor.decompress.Decompressor;
+import com.sarthaks93.file_compressor.utils.AlgorithmUtils;
+import com.sarthaks93.file_compressor.utils.IOUtils;
 
 public class HuffmanCodingDecompressor implements Decompressor {
 
 	@Override
 	public void decompress(String fileName) {
+		System.out.println("Decompress " + fileName);
 		
+		byte[] bytes = IOUtils.readBytesFromFile(fileName);
+		System.out.println(bytes);
 		
+		BitSet bitArray = BitSet.valueOf(bytes);
 		
+		Map<String, String> prefixMap = HuffmanCodingCompressor.myMap;
+		
+		String text = getTextFromBitArray(bitArray, prefixMap);
 		
 	}
 	
+	
+	private String getTextFromBitArray(BitSet bitArray, Map<String, String> prefixMap) {
+		String text = "";
+		
+		Map<String, String> prefixMapReverse = new HashMap<String, String>();
+		for (Map.Entry<String, String> entry : prefixMap.entrySet()) {
+			prefixMapReverse.put(entry.getValue(), entry.getKey());
+		}
+		
+		System.out.println(bitArray.size());
+		
+		int n = AlgorithmUtils.arrayIndex;
+		
+		String code = "";
+		
+		int startIndex = 0, endIndex = 0;
+		
+		while (endIndex < n) {
+			if (bitArray.get(endIndex)) {
+				code += "1";
+			}
+			else {
+				code += "0";
+			}
+			
+			String prefix = prefixMapReverse.get(code); 
+			
+			if (prefix != null) {
+				code = "";
+				startIndex = endIndex + 1;
+				text += prefix;
+			}
+			
+			endIndex++;
+		}
+		
+		
+		return text;
+	}
 	
 
 }
